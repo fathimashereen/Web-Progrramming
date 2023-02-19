36cd42b4db3a0d8202e889a9abcd13d98e60a21a
fathimashereen <116864071+fathimashereen@users.noreply.github.com>
2023-02-19 16:53:50 +0530

    s1

---
+++ co3/database.php
@@ -0,0 +1,44 @@
+<?php
+$host_name="localhost";
+$user_name="root";
+$password="";
+$db_name="demo";
+
+$conn=mysqli_connect($host_name,$user_name,$password,$db_name);
+
+if($conn){
+echo"connection succesfull";
+}
+else{
+echo"unsuscceful";
+}
+
+if(isset($_POST['submit'])){
+ $username=$_POST['username'];
+ $password=$_POST['password'];
+ $cpassword=$_POST['cpassword'];
+ $sql="INSERT INTO login(username,password,confirmpassword)VALUES('$username','$password','$cpassword')";
+
+ $result=mysqli_query($conn,$sql);
+
+if($result){
+echo'<script>alert("succesfully inserted")</script>';
+}
+else{
+echo'<script>alert("unsuccesfully inserted")</script>';
+}
+}
+?>
+
+<html>
+<body>
+<form action="" method="post">
+<h1>login form</h1>
+<input type="text" name="username" placeholder="username"><br><br>
+<input type="password" name="password" placeholder="password"><br><br>
+<input type="password" name="cpassword" placeholder="confirm password"><br><br>
+<input type="submit" value="submit" name="submit">
+
+</form>
+</body>
+</html>
